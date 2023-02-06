9489cda3165108abc0daf54405bc3d5853c13344
岳少杰 <shaojie.yue@beyonca.com>
2023-02-06 11:50:18 +0800

    init

--- deployments/memory-validating-webhook/webhook-create-signed-cert.sh
+++ deployments/memory-validating-webhook/webhook-create-signed-cert.sh
@@ -55,25 +55,6 @@ echo "csrName=${csrName}"
 tmpdir=$(mktemp -d)
 echo "creating certs in tmpdir ${tmpdir} "
 
-#cat <<EOF >> ${tmpdir}/csr.conf
-#[req]
-#req_extensions = v3_req
-#distinguished_name = req_distinguished_name
-#[req_distinguished_name]
-#[ v3_req ]
-#basicConstraints = CA:FALSE
-#keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-#extendedKeyUsage = serverAuth
-#subjectAltName = @alt_names
-#[alt_names]
-#DNS.1 = ${service}
-#DNS.2 = ${service}.${namespace}
-#DNS.3 = ${service}.${namespace}.svc
-#EOF
-#
-#openssl genrsa -out ${tmpdir}/server-key.pem 2048
-#openssl req -new -key ${tmpdir}/server-key.pem -subj "/CN=${service}.${namespace}.svc" -out ${tmpdir}/server.csr -config ${tmpdir}/csr.conf
-
 cat <<EOF | cfssl genkey - | cfssljson -bare server
 {
   "hosts": [
@@ -114,7 +95,6 @@ spec:
   - key encipherment
   - server auth
   signerName: kubernetes.io/kubelet-serving
-#  signerName: my-webhook.example.com/service
   expirationSeconds: 86400  # one day
 EOF
 
